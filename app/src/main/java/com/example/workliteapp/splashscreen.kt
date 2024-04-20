ab9d6648e80585982a755a709c41f35f0f2cd761
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-20 17:02:54 +0530

    login

---
+++ app/src/main/java/com/example/workliteapp/splashscreen.kt
@@ -0,0 +1,24 @@
+package com.example.workliteapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.example.workliteapp.Login.signinActivity
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        // Delay for 2 seconds and then start the main activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, signinActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 2000 milliseconds
+    }
+}
