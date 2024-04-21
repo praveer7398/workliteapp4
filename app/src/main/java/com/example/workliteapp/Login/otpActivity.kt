d3585eab8c3dbedfc09024e366fecabc2099be49
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-21 16:10:14 +0530

    home

---
+++ app/src/main/java/com/example/workliteapp/Login/otpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.workliteapp.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workliteapp.R
+
+class otpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
--- app/src/main/java/com/example/workliteapp/Login/signinActivity.kt
+++ app/src/main/java/com/example/workliteapp/Login/signinActivity.kt
@@ -1,12 +1,30 @@
 package com.example.workliteapp.Login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.workliteapp.R
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.workliteapp.HomeActivity
+import com.example.workliteapp.ProfileActivity
 
-class signinActivity : AppCompatActivity() {
+import com.example.workliteapp.R
+import com.example.workliteapp.RewardActivity
+import com.example.workliteapp.databinding.ActivitySigninBinding
+
+
+class signinActivity : AppCompatActivity(){
+ private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+       binding=ActivitySigninBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+           val intent=Intent(this,RewardActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
     }
 }
