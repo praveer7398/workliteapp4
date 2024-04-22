2c67760e9d57dc042820bdaefcf89ce3918abd07
Apoorva <apoorvasrivastava735@gmail.com>
2024-04-22 17:26:31 +0530

    firebase

--- app/src/main/java/com/example/workliteapp/Login/otpActivity.kt
+++ app/src/main/java/com/example/workliteapp/Login/otpActivity.kt
@@ -1,12 +1,156 @@
 package com.example.workliteapp.Login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.workliteapp.MainActivity
 import com.example.workliteapp.R
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class otpActivity : AppCompatActivity() {
+    private lateinit var auth:FirebaseAuth
+    private lateinit var verifyBtn : Button
+    private lateinit var resendTV : TextView
+    private lateinit var OTP : String
+    private lateinit var inputOTP:EditText
+    private lateinit var resendToken : PhoneAuthProvider.ForceResendingToken
+    private lateinit var phoneNumber : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
+
+        OTP= intent.getStringExtra("OTP").toString()
+        resendToken=intent.getParcelableExtra("resendToken")!!
+        phoneNumber=intent.getStringExtra("phoneNumber")!!
+
+        init()
+        resendOTPvisibility()
+        resendTV.setOnClickListener {
+            resendVerificationCode()
+            resendOTPvisibility()
+        }
+        verifyBtn.setOnClickListener {
+            val typedOTP=inputOTP.toString()
+
+            if (typedOTP.isNotEmpty()){
+                if(typedOTP.length==6){
+                        val credential : PhoneAuthCredential=PhoneAuthProvider.getCredential(
+                            OTP,typedOTP
+                        )
+                    signInWithPhoneAuthCredential(credential)
+                }else{
+                    Toast.makeText(this,"Please Enter Correct OTP", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Please Enter OTP", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun resendOTPvisibility(){
+        inputOTP.setText("")
+        resendTV.visibility= View.INVISIBLE
+        resendTV.isEnabled=false
+        Handler(Looper.myLooper()!!).postDelayed(Runnable {
+            resendTV.visibility=View.VISIBLE
+            resendTV.isEnabled=true
+        },60000)
+    }
+    private fun resendVerificationCode(){
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber) // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+            .setActivity(this) // Activity (for callback binding)
+            .setCallbacks(callbacks)
+            .setForceResendingToken(resendToken)// OnVerificationStateChangedCallbacks
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verification without
+            //     user action.
+
+            signInWithPhoneAuthCredential(credential)
+        }
+
+        override fun onVerificationFailed(e: FirebaseException) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+
+
+            if (e is FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e is FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            } else if (e is FirebaseAuthMissingActivityForRecaptchaException) {
+                // reCAPTCHA verification attempted with null Activity
+            }
+
+            // Show a message and update the UI
+        }
+
+        override fun onCodeSent(
+            verificationId: String,
+            token: PhoneAuthProvider.ForceResendingToken,
+        ) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+
+
+            // Save verification ID and resending token so we can use them later
+            OTP=verificationId
+            resendToken=token
+        }
+
+    }
+    fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this,"Authentication Successful",Toast.LENGTH_SHORT).show()
+                    sendToMain()
+
+                } else {
+                    // Sign in failed, display a message and update the UI
+
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+                        // The verification code entered was invalid
+                    }
+                    // Update UI
+                }
+            }
+    }
+    private fun sendToMain(){
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+    private fun init(){
+        auth=FirebaseAuth.getInstance()
+        verifyBtn=findViewById(R.id.loginButton)
+        resendTV=findViewById(R.id.resendBtn)
+        inputOTP=findViewById(R.id.otpEditText)
     }
 }
