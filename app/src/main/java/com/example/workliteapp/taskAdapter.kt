9ad17033e4bf8c7de5963ded8820439b955ad23b
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-25 00:04:26 +0530

    recycler

---
+++ app/src/main/java/com/example/workliteapp/taskAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.workliteapp
+
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.workliteapp.databinding.LayoutProfileItemBinding
+import com.example.workliteapp.databinding.TaskLayoutitemBinding
+
+class taskAdapter(private val context: Context, private val list: List<TaskItem>) :
+    RecyclerView.Adapter<taskAdapter.CartViewHolder>() {
+
+    inner class CartViewHolder(val binding: TaskLayoutitemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        val binding = TaskLayoutitemBinding.inflate(LayoutInflater.from(context), parent, false)
+        return CartViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        val currentItem = list[position]
+        holder.binding.task1.text = currentItem.tasktitle
+        holder.binding.taskDueDate1.text = currentItem.date
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+}
