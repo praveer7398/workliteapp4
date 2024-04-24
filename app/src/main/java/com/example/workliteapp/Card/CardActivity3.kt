2cb1425babc4ca0b393eea9fb5ad615d843db4a1
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-24 11:24:49 +0530

    cardactivity

---
+++ app/src/main/java/com/example/workliteapp/Card/CardActivity3.kt
@@ -0,0 +1,12 @@
+package com.example.workliteapp.Card
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.workliteapp.R
+
+class CardActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card3)
+    }
+}
--- app/src/main/java/com/example/workliteapp/cardFragment.kt
+++ app/src/main/java/com/example/workliteapp/cardFragment.kt
@@ -6,12 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.workliteapp.Card.CardActivity1
+import com.example.workliteapp.Card.CardActivity2
+import com.example.workliteapp.Card.CardActivity3
+import com.example.workliteapp.Card.CardActivity4
 import com.example.workliteapp.databinding.FragmentCardBinding
 import com.example.workliteapp.databinding.FragmentHomeBinding
 
 
 class cardFragment : Fragment() {
     private lateinit var binding: FragmentCardBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,39 +27,31 @@ class cardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentCardBinding.inflate(layoutInflater)
+        binding = FragmentCardBinding.inflate(inflater, container, false)
 
-        binding.cardview1.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
-            startActivity(intent)
-        }
+        //  retrieveDataFromFirestore()
 
-        binding.cardview2.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
-            startActivity(intent)
-        }
 
-        binding.cardview3.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
-            startActivity(intent)
-        }
+        val intent = Intent(context, CardActivity1::class.java)
+        startActivity(intent)
+
 
         binding.cardview4.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
+            val intent = Intent(context, CardActivity2::class.java)
             startActivity(intent)
         }
 
         binding.cardview5.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
+            val intent = Intent(context, CardActivity3::class.java)
             startActivity(intent)
         }
 
         binding.cardview6.setOnClickListener {
-            val intent = Intent(context,MainActivity::class.java)
+            val intent = Intent(context, CardActivity4::class.java)
             startActivity(intent)
         }
         return (binding.root)
     }
+}
 
 
-    }
