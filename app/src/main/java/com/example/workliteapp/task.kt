34682c960d8edbcff6e17410547cd59d417ea64e
Bhoomi0207 <bhoomiag0207@gmail.com>
2024-04-25 21:46:35 +0530

    firebase

--- app/src/main/java/com/example/workliteapp/task.kt
+++ app/src/main/java/com/example/workliteapp/task.kt
@@ -5,5 +5,6 @@ package com.example.workliteapp
 data class TaskItem(
     val taskName: String,
     val taskdate: String,
+    var isCompleted: Boolean = false,
 
 )
--- app/src/main/java/com/example/workliteapp/taskAdapter.kt
+++ app/src/main/java/com/example/workliteapp/taskAdapter.kt
@@ -4,6 +4,7 @@ package com.example.workliteapp
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.workliteapp.databinding.LayoutProfileItemBinding
 import com.example.workliteapp.databinding.TaskLayoutitemBinding
@@ -12,7 +13,9 @@ class taskAdapter(private val context: Context, private val list: List<TaskItem>
     RecyclerView.Adapter<taskAdapter.CartViewHolder>() {
 
     inner class CartViewHolder(val binding: TaskLayoutitemBinding) :
-        RecyclerView.ViewHolder(binding.root)
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
         val binding = TaskLayoutitemBinding.inflate(LayoutInflater.from(context), parent, false)
@@ -23,6 +26,20 @@ class taskAdapter(private val context: Context, private val list: List<TaskItem>
         val currentItem = list[position]
         holder.binding.task1.text = currentItem.taskName
         holder.binding.taskDueDate1.text = currentItem.taskdate
+        holder.binding.checkbox.isChecked = currentItem.isCompleted
+
+        holder.binding.checkbox.setOnClickListener {
+            val isChecked = holder.binding.checkbox.isChecked
+            if (isChecked) {
+                // Checkbox is checked, show a toast message
+                Toast.makeText(holder.itemView.context, "Task completed!", Toast.LENGTH_SHORT).show()
+            } else {
+                // Checkbox is unchecked, show pending tasks
+              //  showPendingTasks()
+            }
+        }
+
+
 
 
     }
