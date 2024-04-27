2efdbf9560021e4003c9e5bd795285eea98c9e00
Apoorva Srivastava <122932821+ApoorvaSri123@users.noreply.github.com>
2024-04-28 01:29:27 +0530

    git

--- app/src/main/java/com/example/workliteapp/ProfileFragment.kt
+++ app/src/main/java/com/example/workliteapp/ProfileFragment.kt
@@ -27,20 +27,20 @@ class ProfileFragment : Fragment() {
 
         val profileItems = listOf(
 
-            ProfileItem("Meena",  "7706017030", R.drawable.woman12),
-            ProfileItem("Deepa","6398581017", R.drawable.woman13),
-            ProfileItem("Sonam", "9084243046", R.drawable.woman14),
-            ProfileItem("Mahima", "8318427641", R.drawable.woman15),
-            ProfileItem("Pratibha", "9015768567", R.drawable.woman1),
-            ProfileItem("Ragini", "8874356772", R.drawable.woman2),
-            ProfileItem("Sushila", "6954302105", R.drawable.woman4),
-            ProfileItem("Naina",  "6392134091", R.drawable.woman5),
-            ProfileItem("Vineeta","8764592061", R.drawable.woman6),
-            ProfileItem("Poonam", "8313750129", R.drawable.woman7),
-            ProfileItem("Sunita", "6205672349", R.drawable.woman8),
-            ProfileItem("Rohini", "8299789338", R.drawable.woman9),
-            ProfileItem("Sapna", "9693265100", R.drawable.woman10),
-            ProfileItem("Pinki", "7884356013", R.drawable.woman16),
+            ProfileItem("Meena(मीना)",  "7706017030", R.drawable.woman12),
+            ProfileItem("Deepa(दीपा)","6398581017", R.drawable.woman13),
+            ProfileItem("Sonam(सोनम)", "9084243046", R.drawable.woman14),
+            ProfileItem("Mahima(महिमा)", "8318427641", R.drawable.woman15),
+            ProfileItem("Pratibha(प्रतिभा)", "9015768567", R.drawable.woman1),
+            ProfileItem("Ragini(रागिनी)", "8874356772", R.drawable.woman2),
+            ProfileItem("Sushila(सुशीला)", "6954302105", R.drawable.woman4),
+            ProfileItem("Naina(नैना)",  "6392134091", R.drawable.woman5),
+            ProfileItem("Vineeta(विनीता)","8764592061", R.drawable.woman6),
+            ProfileItem("Poonam(पूनम)", "8313750129", R.drawable.woman7),
+            ProfileItem("Sunita(सुनीता)", "6205672349", R.drawable.woman8),
+            ProfileItem("Rohini(रोहिणी)", "8299789338", R.drawable.woman9),
+            ProfileItem("Sapna(सपना)", "9693265100", R.drawable.woman10),
+            ProfileItem("Pinki(पिंकी)", "7884356013", R.drawable.woman16),
             // Add more ProfileItems as needed
         )
 
